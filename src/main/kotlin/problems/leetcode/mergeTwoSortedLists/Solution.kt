ac0b6b99d96f44b5915cbc0fb23a4694f962a3d5
smallzooDev <smallzooDev@gmail.com>
2025-01-30 10:45:13 +0900

    leetcode palindrome linked list

--- src/main/kotlin/problems/leetcode/mergeTwoSortedLists/Solution.kt
+++ src/main/kotlin/problems/leetcode/mergeTwoSortedLists/Solution.kt
@@ -29,4 +29,17 @@ class Solution {
 
         return dummy.next
     }
+
+    fun mergeTwoLists2(list1: ListNode?, list2: ListNode?): ListNode? {
+        if (list1 == null) return list2
+        if (list2 == null) return list1
+
+        return if (list1.`val` < list2.`val`) {
+            list1.next = mergeTwoLists2(list1.next, list2)
+            list1
+        } else {
+            list2.next = mergeTwoLists2(list1, list2.next)
+            list2
+        }
+    }
 }
