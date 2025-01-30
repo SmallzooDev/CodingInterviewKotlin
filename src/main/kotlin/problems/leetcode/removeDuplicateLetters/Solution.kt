39580369d5ad03b2338eef3aa9c711b17a26fb67
smallzooDev <smallzooDev@gmail.com>
2025-01-30 16:43:53 +0900

    leetcode stack

--- src/main/kotlin/problems/leetcode/removeDuplicateLetters/Solution.kt
+++ src/main/kotlin/problems/leetcode/removeDuplicateLetters/Solution.kt
@@ -11,8 +11,14 @@ class Solution {
         }
 
         for (char in s) {
+            // 현재 문자를 사용했으므로 등장 횟수 감소
             letterCountMap[char] = letterCountMap[char]!! - 1
+
+            // 이미 스택에 있는 문자는 스킵 (중복을 허용하지 않음)
             if (inStack.contains(char)) continue
+
+            // 스택의 마지막 문자가 현재 문자보다 사전순으로 뒤에 있고,
+            // 뒤에 다시 나올 수 있다면 해당 문자를 스택에서 제거
             while (stack.isNotEmpty() && stack.last() > char && letterCountMap[stack.last()]!! > 0) {
                 inStack.remove(stack.removeLast())
             }
@@ -21,6 +27,7 @@ class Solution {
             inStack.add(char)
         }
 
+        // 스택에 저장된 결과를 문자열로 변환하여 반환
         return stack.joinToString("")
     }
 }
