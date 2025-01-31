a81dd355b5b9200f55705d04f319fee510f81492
smallzooDev <smallzooDev@gmail.com>
2025-01-31 18:57:12 +0900

    leetcode bfs

--- src/main/kotlin/problems/leetcode/numberOfIslands/Solution.kt
+++ src/main/kotlin/problems/leetcode/numberOfIslands/Solution.kt
@@ -1,5 +1,8 @@
 package problems.leetcode.numberOfIslands
 
+import java.util.LinkedList
+import java.util.Queue
+
 class Solution {
     val dy = intArrayOf(1, 0, -1, 0)
     val dx = intArrayOf(0, 1, 0, -1)
@@ -30,6 +33,26 @@ class Solution {
             val ny = y + dy[i]
             val nx = x + dx[i]
             dfs(grid, visited, ny, nx)
+        }
+    }
+
+    private fun bfs(grid: Array<CharArray>, visited: Array<IntArray>, startY: Int, startX: Int) {
+        val queue: Queue<Pair<Int, Int>> = LinkedList()
+        queue.add(Pair(startY, startX))
+        visited[startY][startX] = 1
+
+        while (queue.isNotEmpty()) {
+            val (y, x) = queue.poll()
+            for (i in 0 until 4) {
+                val ny = y + dy[i]
+                val nx = x + dx[i]
+
+                if (ny in grid.indices && nx in grid[0].indices && grid[ny][nx] == '1' && visited[ny][nx] == 0) {
+                    visited[ny][nx] = 1
+                    queue.add(Pair(ny, nx))
+                    queue.add(Pair(ny, nx))
+                }
+            }
         }
     }
 }
