15c95252bb2e2728dd8faa0cd5eb3c22963aa09a
SmallzooDev <smallzooDev@gmail.com>
2025-04-30 10:50:28 +0900

    b11650

---
+++ src/main/kotlin/problems/baekjoon/p23246/Main.kt
@@ -0,0 +1,38 @@
+package problems.baekjoon.p23246
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+data class Climber(
+    val id: Int,
+    val lead: Int,
+    val speed: Int,
+    val bouldering: Int,
+): Comparable<Climber> {
+    private val total: Int
+        get() = lead * speed * bouldering
+    private val sum: Int
+        get() = lead + speed + bouldering
+
+    override fun compareTo(other: Climber): Int {
+        return compareValuesBy(this, other,
+            {it.total}, {it.sum}, {it.id}
+        )
+    }
+}
+
+
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().toInt()
+    val climbers: MutableList<Climber> = mutableListOf()
+    repeat(n) {
+        val tmp = readLine()!!.split(" ").map { it.toInt() }
+        climbers.add(Climber(tmp[0], tmp[1], tmp[2], tmp[3]))
+    }
+    climbers.sorted()
+    val sortedClimbers = climbers.sorted()
+    println(sortedClimbers.take(3).joinToString(" ") { it.id.toString() })
+    close()
+}
+
---
+++ src/main/kotlin/problems/baekjoon/p23246/README.md
@@ -0,0 +1,3 @@
+# baekjoon p23246
+---
+> url : https://www.acmicpc.net/problem/23246
