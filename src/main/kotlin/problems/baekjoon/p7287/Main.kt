63f2c4995432a4be11c27e60fea70f7aa8748901
smallzooDev <smallzooDev@gmail.com>
2025-06-01 12:37:00 +0900

    first commit

---
+++ src/main/kotlin/problems/baekjoon/p7287/Main.kt
@@ -0,0 +1,10 @@
+package problems.baekjoon.p7287
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    println(126)
+    println("marlcocoro")
+    close()
+}
---
+++ src/main/kotlin/problems/baekjoon/p7287/README.md
@@ -0,0 +1,3 @@
+# baekjoon p7287
+---
+> url : https://www.acmicpc.net/problem/7287
